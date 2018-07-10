b7de3264e7f03c86bfc875b92f8a5a5467f6a407
Bebnev Anton <bambr.ab@gmail.com>
2018-07-10 11:55:42 +0300

    change ios project type

--- ios/RNUserAgent/RNUserAgent/RNUserAgent-Bridging-Header.h
+++ ios/RNUserAgent/RNUserAgent/RNUserAgent-Bridging-Header.h
@@ -2,6 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
---
+++ ios/RNUserAgent/RNUserAgent/RNUserAgent.m
@@ -0,0 +1,14 @@
+//
+//  RNUserAgent.m
+//  RNUserAgent
+//
+//  Created by Anton Bebnev on 10.07.2018.
+//  Copyright © 2018 Anton Bebnev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(RNUserAgent, NSObject)
+
+@end
